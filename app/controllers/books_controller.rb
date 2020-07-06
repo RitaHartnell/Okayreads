5330ec35ab2847d005a8eb53662f7dc19d09baf7
dr-dolce14 <dolcemascop1@gmail.com>
2020-07-06 19:10:39 -0400

    added books api and search results page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,22 @@ class BooksController < ApplicationController
     def show
         @book = Book.find(params[:id])
     end
+
+    def search
+    books = find_book(params[:title])
+    @arrayOfBooks
+    end
+
+    private
+
+    def find_book(title)
+        url = "https://www.googleapis.com/books/v1/volumes?q=#{(params[:title])}"
+        restClientResponseObject = RestClient.get(url)
+        jsonButItsAString = restClientResponseObject.body
+        workable_hash = JSON.parse(jsonButItsAString)   
+        @arrayOfBooks = workable_hash["items"]
+    end
+
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  
   resources :reviews, only: [:show, :create, :new, :edit, :update, :delete]
   resources :authors, only: [:index, :show]
   resources :books, only: [:index, :show]
   resources :users, only: [:show, :new, :create, :edit, :update]
+  root to: 'books#index'
+  get '/search' => 'books#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
