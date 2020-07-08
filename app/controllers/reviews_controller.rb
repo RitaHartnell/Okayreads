4b739e678c45bb802b1feabcab0a9140d3a1076e
RitaHartnell <mel.soriano6123@gmail.com>
2020-07-08 13:45:28 -0400

    added some cookies

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,6 +9,9 @@ class ReviewsController < ApplicationController
 
     def create
         @review = Review.new(review_params)
+        if session[:book_id]
+            @review.book_id = session[:book_id]
+        end
         @review.save
         redirect_to review_path(@review)
     end
@@ -26,6 +29,10 @@ class ReviewsController < ApplicationController
     private
 
     def review_params
-        params.require(:review).permit(:title, :user_id, :book_id, :content, :rating)
+        if !session[:book_id]
+            params.require(:review).permit(:title, :user_id, :book_id, :content, :rating)   
+        else
+            params.require(:review).permit(:title, :user_id, :content, :rating)
+        end
     end
 end
