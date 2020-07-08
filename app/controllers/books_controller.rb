d22a5627e99b11e2e2c4fda3712bf83474cceb06
RitaHartnell <mel.soriano6123@gmail.com>
2020-07-08 09:52:28 -0400

    tabs

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,17 +8,16 @@ class BooksController < ApplicationController
     end
 
     def search
-    @books = find_book(params[:title])
-    @arrayOfBooks
-    @books.each do |book_hash|
-        book_title = book_hash["volumeInfo"]["title"]
-        book_author = book_hash["volumeInfo"]["authors"] && book_hash["volumeInfo"]["authors"].first
-     Book.save_it(book_title, book_author)
-    end
+        @books = find_book(params[:title])
+        @arrayOfBooks
+        @books.each do |book_hash|
+            book_title = book_hash["volumeInfo"]["title"]
+            book_author = book_hash["volumeInfo"]["authors"] && book_hash["volumeInfo"]["authors"].first
+            Book.save_it(book_title, book_author)
+        end
    end
-
-
-      private
+   
+    private
 
     def find_book(title)
         url = "https://www.googleapis.com/books/v1/volumes?q=#{(params[:title])}"
