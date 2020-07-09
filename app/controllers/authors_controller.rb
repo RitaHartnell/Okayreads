658a477b98fa6d1a68bf59c1344cff626021998f
dr-dolce14 <dolcemascop1@gmail.com>
2020-07-09 12:20:24 -0400

    finished favorites

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,6 +2,7 @@ class AuthorsController < ApplicationController
     skip_before_action :auth_user, only: [:show, :index]
     def show
         @author = Author.find(params[:id])
+        session[:author_id] = params[:id]
     end
 
     def index
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ skip_before_action :auth_user, only: [:login, :create, :index]
 
     def create
         user = User.find_by(username: params[:session][:username])
-        if user.authenticate(params[:session][:password])
+        if user && user.authenticate(params[:session][:password])
             session[:user_id] = user.id 
             redirect_to user 
         else   
