657b9ac66eb02567c6a6a6ca9790da0ffd5aa4e9
RitaHartnell <mel.soriano6123@gmail.com>
2020-07-10 00:07:09 -0400

    fav/unfav from author page

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,8 @@ Rails.application.routes.draw do
   get '/sessions/new', to: 'sessions#login', as: 'new_login'
   post '/sessions/', to: 'sessions#create', as: 'sessions'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  post '/authors/:id/favorite', to: "authors#favorite", as: "favorite_author"
+  post '/authors/:id/unfavorite', to: "authors#unfavorite", as: "unfavorite_author"
 end
